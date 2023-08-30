2ecfb6b31d09e05d01a26ad7a59d61df4fe72e74
Nesssu <80637080+Nesssu@users.noreply.github.com>
2023-08-30 20:30:18 +0300

    #3: Deleted all the useless items from the client project and created a rought frame for the homepage.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="NavBar">
+        My Vinyl Collection
+      </div>
+
+      <div className="CollectionBackground">
+        The collection will be here
+      </div>
+
+      <div className="Footer">
+        Made by Nestori Kangashaka
+      </div>
     </div>
   );
 }

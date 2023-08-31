d64e1d14e6bd4e676876a275cef69a893509ebac
Nesssu <80637080+Nesssu@users.noreply.github.com>
2023-08-31 19:24:14 +0300

    #1: The server is now initialized

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,44 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/*
+
+This is where all the request are made to send and retrieve the vinyl
+data from the database.
+
+*/
+
+
+// Get all the vinyl record data and pictures from the database.
+router.get('/record/all/', (req, res, next) =>
+{
+  return res.json({
+    message: "All the records"
+  });
+});
+
+// Add new record to the database.
+router.post('/record/add/', (req, res, next) => 
+{
+  return res.json({
+    message: "New record added!"
+  });
+});
+
+// Edit record by it's name and artist
+router.put('/record/update/', (req, res, next) =>
+{
+  return res.json({
+    message: "Record updated!"
+  });
+});
+
+// Remove a record based on it's name and artist.
+router.delete('/record/delete/', (req, res, next) =>
+{
+  return res.json({
+    message: "Record deleted!"
+  });
 });
 
 module.exports = router;

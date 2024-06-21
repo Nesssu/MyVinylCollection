7298d9412f970da0be9d03d8a2e5988ec6acf42f
Nesssu <80637080+Nesssu@users.noreply.github.com>
2024-06-21 12:23:06 +0300

    Records updated. Added a blur when the user hovers on them and the artist and title are only shwon then.

--- client/src/components/Record.js
+++ client/src/components/Record.js
@@ -1,16 +1,18 @@
 import '../App.css';
+import { useState, useEffect } from 'react';
 
 const Record = (props) =>
 {
+   const [hovering, setHovering] = useState(false);
+
   return (
-    <div className='RecordBackground'>
-      <div className='RecordImage' style={{backgroundImage: `url(data:${props.contentType};base64,${props.image})`}}/>
-      <div className='HorizontalSeparator' style={{width: "90%", backgroundColor: "#CABA9C"}} />
-      <div className='RecordInfoArea'>
+    <div className='RecordBackground' onMouseEnter={() => setHovering(true)} onMouseLeave={() => setHovering(false)}>
+      <div className='RecordImage' style={{backgroundImage: `url(data:${props.contentType};base64,${props.image})`, filter: hovering && 'blur(8px)'}}/>                                                                       
+      <div className='RecordInfoArea' style={{opacity: hovering ? '1' : '0' }}>
         <p># {props.number}</p>
         <p>{props.artist}</p>
-        <div style={{position: "relative", width: "100%", height: "100%", display: "flex", justifyContent: "center", margin: "0 0 20px 0"}} >
-          <p style={{position: "absolute"}} >{props.title}</p>
+        <div style={{width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}} >
+          <p>{props.title}</p>
         </div>
       </div>
     </div>
